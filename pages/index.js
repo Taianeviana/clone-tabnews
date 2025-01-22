93d33a1c177a7f00fb9c69f93caad6d8a94f72f8
Taianeviana <32682339+Taianeviana@users.noreply.github.com>
2025-01-22 01:05:35 +0000

    update text in the page index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Hello Word!</h1>
+    return (
+        <>
+        <h1>Hello Word!</h1>
+        <p>Meu primeiro pedacinho da internet.</p>
+        </>
+    
+    );
 }
 
 export default Home;
